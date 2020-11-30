54adc6f27079f28c87d86d163f725cfbeb0f8d5e
luffyZh <zhoudeyou@kuaishou.com>
2020-11-30 10:26:45 +0800

    adjust path

--- utils/autoCss2Str.js
+++ utils/autoCss2Str.js
@@ -8,20 +8,20 @@ const fs = require('fs');
  */
 function getFileStat(path){
   return new Promise((resolve, reject) => {
-      fs.stat(path, (err, stats) => {
-          if(err){
-              resolve(false);
-          }else{
-              resolve(stats);
-          }
-      })
-  })
+    fs.stat(path, (err, stats) => {
+      if(err) {
+        resolve(false);
+      } else {
+        resolve(stats);
+      }
+    });
+  });
 }
 
 // 输入的 theme.css
-const inFilePath = path.join(`${__dirname}/src/core/theme.css`);
+const inFilePath = path.join(`${__dirname}/../src/core/theme.css`);
 // 输出的 theme.js
-const outFilePath = path.join(`${__dirname}/src/core/theme.js`);
+const outFilePath = path.join(`${__dirname}/../src/core/theme.js`);
 
 /**
  * @description 先删除再创建，保证每次都是最新的css文件
@@ -34,7 +34,7 @@ async function intialCss2JsFile() {
   }
   // css 字符串
   const cssStr = css2str(fs.readFileSync(inFilePath));
-  const inStr = `module.exports = \`\t${cssStr}\r\n\`;`;
+  const inStr = `module.exports = \`\r\n\t${cssStr}\r\n\`;`;
   // 删除之后重新创建一个空文件
   fs.writeFileSync(outFilePath, inStr);
 }
